37bce6fa13928218a9747d4f0ae43e81755a9998
Mats <matssa@stud.ntnu.no>
2016-09-06 16:43:10 +0200

    Add files via upload

---
+++ oving2.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+
+from sys import stdin, stderr
+import traceback
+
+
+class Node:
+    def __init__(self):
+        self.barn = {}
+        self.posi = []
+
+
+def bygg(ordliste):
+    toppNode = Node()
+    return byggTre(toppNode, ordliste)
+
+def byggTre(topp, ordliste):
+
+    print(ordliste)                         #må bort etterpaa
+
+
+def posisjoner(ord, indeks, node):
+    x = 0
+
+
+def main():
+    try:
+        ord = stdin.readline().split()
+        ordliste = []
+        pos = 0
+        for o in ord:
+            ordliste.append((o, pos))
+            pos += len(o) + 1
+        toppnode = bygg(ordliste)
+        for sokeord in stdin:
+            sokeord = sokeord.strip()
+            print("%s:" % sokeord, end='')
+            posi = posisjoner(sokeord, 0, toppnode)
+            posi.sort()
+            for p in posi:
+                print(" %s" % p, end='')
+            print()
+    except:
+        traceback.print_exc(file=stderr)
+
+
+if __name__ == "__main__":
+    main()
