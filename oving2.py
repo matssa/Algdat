bc6934a0a4ed0eaf50aa20599fa8ea185aa5a7f4
Mats <matssa@stud.ntnu.no>
2016-09-08 13:39:38 +0200

    Add files via upload

--- oving2.py
+++ oving2.py
@@ -9,12 +9,6 @@ class Node:
         self.barn = {}
         self.posi = []
 
-def printTree(node, thisKey):
-    print(thisKey, end=' ')
-    print(node.posi)
-    for key in node.barn:
-        printTree(node.barn[key], key)
-
 def buildTree(node, word):
     letter = word[0]
     word = word[1:]
@@ -29,19 +23,39 @@ def bygg(ordliste):
     for tuppel in ordliste:
         endNode = buildTree(toppNode, tuppel[0])
         endNode.posi.append(tuppel[1])
-    printTree(toppNode, 'toppNode')
     return toppNode
 
 def posisjoner(ord, indeks, node):
+    pos = []
     for letter in ord:
+        pos.sort()
         if letter in node.barn:
             node = node.barn[letter]
-        if letter == '?':
-            for unge in node.barn:
-                if ord[indeks] == 
-
-        indeks +=1
-    return node.posi
+            indeks += 1
+            if indeks == len(ord):
+                for i in node.posi:
+                    pos.append(i)
+                return pos
+        elif letter == '?':
+            indeks += 1
+            if indeks == len(ord):
+                for b in node.barn:
+                    barnet = node.barn[b]
+                    for i in barnet.posi:
+                        pos.append(i)
+                return pos
+            for i in node.barn:
+                index = indeks
+                barnet = node.barn[i]
+                if ord[index] in barnet.barn:
+                    while index != len(ord):
+                        barnet = barnet.barn[ord[index]]
+                        index += 1
+                    for p in barnet.posi:
+                            pos.append(p)
+                if pos:
+                    return pos
+    return pos
 
 def main():
     try:
