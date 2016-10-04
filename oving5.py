cd16015d3ff361e9249185957f7034829b504f75
Mats <matssa@stud.ntnu.no>
2016-10-04 15:37:51 +0200

    lagd en fil med eksempelinput

--- oving5.py
+++ oving5.py
@@ -1,15 +1,17 @@
-from sys import stdin
+#from sys import stdin                                  #her må det endres før innlevering
 from string import ascii_lowercase as chars
 from random import randint, choice
 from operator import itemgetter
 from collections import defaultdict
 
 
-def flexradix(A, d):    # d = antall strenger
-    # idé: hva med å dele opp strengene etter lengde for å samle de av lik lengde og merge sammen etterpå? Hmm, får kanskje kjøretid på Ø(n log n)...
 
+def flexradix(A, d):    # d = lengden på den lengste strengen
+    # idé: hva med å dele opp strengene etter lengde for å samle de av lik lengde og merge sammen etterpå? Hmm, får kanskje kjøretid på Ø(n log n)...
+    return ["denne", "lista", "skal", "sorteres"] # tull
 
 def main():
+    stdin = open('eksInput.txt', 'r')           #yolo
     d = int(stdin.readline())
     strings = []
     for line in stdin:
