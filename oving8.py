a1625848086d434289a31021ea4425aede496bed
Mats <matssa@stud.ntnu.no>
2016-10-31 13:38:38 +0100

    bkldfm

--- oving8.py
+++ oving8.py
@@ -3,38 +3,57 @@ import math
 
 
 def min_coins_greedy(coins, value, money):
-    print('greedy: ')
     temp = 0
     for coin in coins:
-        if value > coin:
+        if value >= coin:
             temp = math.floor(value / coin)
             value = value % coin
             money += temp
     return money
 
 
+def dynamic(coins, value):
+    m = min_coins_dynamic(coins, value)
+    for c in range(1, len(coins) + 1):
+        for r in range(1, value + 1):
+            if coins[c - 1] == r:
+                m[c][r] = 1
+            elif coins[c - 1] > r:
+                m[c][r] = m[c - 1][r]
+            else:
+                m[c][r] = min(m[c - 1][r], 1 + m[c][r - coins[c - 1]])
+    return m[-1][-1]
+
+# Lager en matrise
 def min_coins_dynamic(coins, value):
-    print('dynamic')
-    temp = 0
-    money = 0
-    for coin in coins:
-        if value > coin:
-            temp = math.floor(value / coin)
-            value = value % coin
-            money += temp
-    return money
+    m = [[0 for _ in range(value + 1)] for _ in range(len(coins) + 1)]
+    for i in range(value + 1):
+        m[0][i] = i
+    return m
 
 
 def can_use_greedy(coins, value, money):
-    greedy = True
+    greedy1 = True
     for i in range(len(coins)-1):
         if coins[i] % coins[i+1] != 0:
-            greedy = False
+            greedy1 = False
             break
-    if greedy:
+    if greedy1:
         return min_coins_greedy(coins, value, money)
     else:
-        return min_coins_dynamic(coins, value)
+        fromGreedy = 0
+        temp = 0
+        x = len(coins) - 1
+        for i in range(len(coins) - 1):
+            if value >= coins[i]:
+                if coins[i] % coins[i+1] == 0:
+                    temp = math.floor(value / coins[i])
+                    value = value % coins[i]
+                    fromGreedy += temp
+                    x -= 1
+                else:
+                    return (dynamic(coins, value) + fromGreedy)
+        return (dynamic(coins, value) + fromGreedy)
 
 coins = []
 for c in stdin.readline().split():
@@ -42,16 +61,14 @@ for c in stdin.readline().split():
 coins.sort()
 coins.reverse()
 method = stdin.readline().strip()
-if method == "dynamisk":
+if method == "graadig":
     for line in stdin:
         money = 0
-        print(min_coins_dynamic(coins, int(line)))
+        print(min_coins_greedy(coins, int(line), money))
+elif method == "dynamisk":
+    for line in stdin:
+        print(dynamic(coins, int(line)))
 else:
-    if method == "graadig":
-        for line in stdin:
-            money = 0
-            print(min_coins_greedy(coins, int(line), money))
-    else:
-        for line in stdin:
-            money = 0
-            print(can_use_greedy(coins, int(line), money))
+    for line in stdin:
+        money = 0
+        print(can_use_greedy(coins, int(line), money))
