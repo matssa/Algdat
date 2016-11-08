9d684ee5bed512020fc995b69b68bdd5b24a852c
matssa <matssa@stud.ntnu.no>
2016-11-08 19:36:38 +0100

    omg

--- oving10.py
+++ oving10.py
@@ -1,15 +1,44 @@
 from sys import stdin
 
-Inf = float(1e3000)
+Inf = float('inf')
+
 
 def mst(nm):
-    # SKRIV DIN KODE HER
+    mintre = prim(nm)
+    summ = 0
+    for (fra, til) in mintre:
+        print('hei')
+        summ = max(summ, nm[fra][til])
+    return summ
 
+def prim(nm):
+    n = len(nm)
+    tre = []
+    bestenabo = [None] * n
+    bestepris = [Inf] * n
+    ikke_funnet = range(1, n)
+    forrige = 0
+    while len(ikke_funnet) > 0:
+        for i in ikke_funnet:
+            if nm[i][forrige] < bestepris[i]:
+                bestenabo[i] = forrige
+                bestepris[i] = nm[i][forrige]
+        minpris = Inf
+        for i in ikke_funnet:
+            if bestepris[i] < minpris:
+                nestefra = i
+                nestetil = bestenabo[i]
+                minpris = bestepris[i]
+        tre.append( (nestefra,nestetil) )
+        ikke_funnet.remove(nestefra)
+        forrige = nestefra
+    return tre
 
 lines = []
 for str in stdin:
     lines.append(str)
 n = len(lines)
+print('bo')
 neighbour_matrix = [None] * n
 node = 0
 for line in lines:
